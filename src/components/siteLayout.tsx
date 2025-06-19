3542730a6fd0045e75a7abffece959dbe9ed4925
alieron <sebastien.leib1@gmail.com>
2025-06-20 04:56:09 +0800

    Fix sidebars and preserve scrolling of the center column

--- src/components/siteLayout.tsx
+++ src/components/siteLayout.tsx
@@ -6,10 +6,10 @@ export default function SiteLayout() {
   return (
     <div className="container flex flex-col max-w-300 mx-auto">
       <SiteHeader />
-      <div className="flex flex-1 overflow-hidden gap-4 py-6 px-4">
+      <div className="flex gap-4 px-4 py-6">
         {/* Left Sidebar */}
-        <div className="w-64 overflow-y-auto">
-
+        <div className="sticky top-20 h-fit w-64 self-start">
+          <p>test</p>
         </div>
 
         {/* Main Content */}
@@ -18,8 +18,8 @@ export default function SiteLayout() {
         </div>
 
         {/* Right Sidebar - File Properties */}
-        <div className="w-64 overflow-y-auto">
-
+        <div className="sticky top-20 h-fit w-64 self-start">
+          <p>test</p>
         </div>
       </div>
     </div>
