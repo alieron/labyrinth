6557117528b0fa1cac551026541af5d30b7462ba
alieron <sebastien.leib1@gmail.com>
2025-06-19 18:33:35 +0800

    Add theme context

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ThemeProvider from "./contexts/themeContext";
 import Home from "./pages/home";
 import SlugPage from "./pages/[...slug]";
 import "./index.css";
@@ -8,10 +9,12 @@ import "./index.css";
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="notes/*" element={<SlugPage />} />
-      </Routes>
+      <ThemeProvider>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="notes/*" element={<SlugPage />} />
+        </Routes>
+      </ThemeProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
