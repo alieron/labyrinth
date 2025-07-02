2e0c4c24664bf4851635ded9afe41bc6f5ebb081
alieron <sebastien.leib1@gmail.com>
2025-07-02 14:43:47 +0800

    Modify 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,11 +1,16 @@
 import { Link } from 'react-router-dom';
+import { Separator } from '@/components/ui/separator';
 
 export default function NotFound() {
   return (
-    <div className="p-8 text-center">
+    <div className="flex flex-col p-8 text-center items-center">
       <h1 className="text-4xl font-bold">404</h1>
       <p className="text-lg">Page not found</p>
-      <Link to="/" className="text-blue-500 underline mt-4 inline-block">Go home</Link>
+      <div className="flex h-5 w-48 items-center space-x-4 mt-8">
+        <Link to="/" className="grow-1 text-primary underline">Home</Link>
+        <Separator orientation="vertical" />
+        <Link to="/notes" className="grow-1 text-primary underline">Notes</Link>
+      </div>
     </div>
   );
 }
