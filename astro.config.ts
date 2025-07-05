ca60d21608af4a6ef0a9b75bed315b6d1d13a174
alieron <sebastien.leib1@gmail.com>
2025-07-05 22:52:09 +0800

    Add custom rehype plugin for tables

--- astro.config.ts
+++ astro.config.ts
@@ -3,6 +3,7 @@ import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
 // Custom plugins
 import { rehypeTypography } from './src/plugins/typography';
+import { rehypeMdTable } from './src/plugins/table';
 
 // https://astro.build/config
 export default defineConfig({
@@ -10,6 +11,7 @@ export default defineConfig({
   markdown: {
     rehypePlugins: [
       rehypeTypography,
+      rehypeMdTable,
     ]
   },
   server: {
