da7c10b33cc6dd78f438f8809fd7305f4a22569a
alieron <sebastien.leib1@gmail.com>
2025-07-15 17:23:01 +0800

    Add custom slug function and path props to dynamic pages

---
+++ src/utils/slug.ts
@@ -0,0 +1,14 @@
+export function trimRelPath(str: string | undefined): string | undefined {
+  return str && str
+    .replace(/^(content\/)/, "") // Trim content directory
+    .replace(/(\.md)$/, ""); // Trim .md extension
+}
+
+export function toSlug(str: string): string {
+  return str
+    .trim()
+    .replace(/\s+/g, '_') // Replace spaces with underscores
+    .replace(/[\u0000-\u001F\u007F]+/g, '') // Remove control characters
+    // .replace(/[^\w\-&³]/g, '') // Optional: remove unwanted characters
+    .replace(/-{2,}/g, '-'); // Collapse multiple dashes
+}
