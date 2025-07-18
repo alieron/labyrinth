34e45a405d0ebd0df43ab86cba3df908bb4cec05
Seb_Leib <sebastien.leib1@gmail.com>
2025-07-18 16:54:51 +0800

    Add search button to header

---
+++ src/components/SearchDialog.tsx
@@ -0,0 +1,10 @@
+import { Search } from "lucide-react";
+import { Button } from "@/components/ui/button";
+
+export default function SearchDialog() {
+  return (
+    <Button size="icon" variant="ghost">
+      <Search />
+    </Button>
+  );
+}
